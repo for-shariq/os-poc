f73ef058f912858e21f1543b97a13c44edea8b46
forshariq <forshariq@gmail.com>
2014-12-26 17:44:02 +0530

    mvc angular

--- Poc/Controllers/ProductController.cs
+++ Poc/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,8 +15,26 @@ namespace Poc.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return View("Index",null,GetProducts());
         }
 
+        public string GetProducts()
+        {
+            var products = new[] 
+                {
+                    new Product { Name = "Nokia", Price = "12.22"},
+                    new Product { Name = "Samsung", Price = "10.22"},
+                    new Product { Name = "HTC", Price = "11.22"}
+                };
+            var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+            return JsonConvert.SerializeObject(products,Formatting.None,settings);
+        }
+
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
     }
 }
